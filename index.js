41ceca60e34879f9c13a8c94dde11c86bfc58725
brighton.flemming <brighton.flemming@student.moringaschool.com>
2023-06-15 14:32:09 +0300

    Mission passed

--- index.js
+++ index.js
@@ -1,1 +1,15 @@
-// code your solution here
+function saturdayFun (activity = 'roller-skate') {
+    return `This Saturday, I want to ${activity}!`;
+} 
+saturdayFun("bathe my dog!");
+
+function mondayWork(activity = 'go to the office') {
+return `This Monday, I will ${activity}.`;
+}
+mondayWork("work from home.");
+
+function wrapAdjective (style ="*"){
+ return function (specialWord = "special"){
+ return `You are ${style}${specialWord}${style}!`
+ }
+}
